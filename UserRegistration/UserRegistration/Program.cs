57a8c6541092ea900e5f420f65ad7d406c107ef8
komalkashyap <komalkashyapmajri403@gmail.com>
2021-12-13 15:09:18 +0530

    [KOMAL KASHYAP] Added: Valid first Name

---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+      class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration Validating using Regular Expression!");
+            RegexDemo regexSample = new RegexDemo();
+            regexSample.Validating();
+        }
+    }
+}
